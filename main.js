74dc015ea3aff5d62ab9b86670322ac57b8056ac
Patrick Brentlinger <pbrentlinger@gmail.com>
2022-05-24 11:34:45 -0400

    Set grid up

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Create a webpage with a 16x16 grid of square divs.
 //     Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
 //     It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
-// There are several different ways to make the divs appear as a grid (versus just one on each line). Feel free to use any or play with each of them:
+// There are several different ways to make the divs appear as a grid (versus just one on each line).
+// Feel free to use any or play with each of them:
 //     float/clear
 // inline-block
 // flexbox
@@ -16,7 +17,7 @@ function makeGrid(size){
     for (let rows = 0; rows < size; rows++){
         const newRow = document.createElement("div");
         newRow.className = "gridRow";
-        
+
         for (let divs = 0; divs < size; divs++){
             const newCell = document.createElement("div");
             newCell.className = "cell";
